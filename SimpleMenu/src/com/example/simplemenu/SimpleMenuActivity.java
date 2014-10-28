782771e5d809a111878e5db7558cecdd28dcfe33
Joanne Caroll <joannecaroll@UNIT001.(none)>
2014-10-28 16:56:51 +0800

    Simple Context Menu implemented

--- SimpleMenu/src/com/example/simplemenu/SimpleMenuActivity.java
+++ SimpleMenu/src/com/example/simplemenu/SimpleMenuActivity.java
@@ -1,21 +1,38 @@
 package com.example.simplemenu;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class SimpleMenuActivity extends ActionBarActivity {
-
-	TextView menuLabel;
+	
+	final int MENU_COLOR_RED = 1;
+	final int MENU_COLOR_GREEN = 2;
+	final int MENU_COLOR_BLUE = 3;
+	final int MENU_SIZE_22 = 4;
+	final int MENU_SIZE_26 = 5;
+	final int MENU_SIZE_30 = 6;
+	
+	TextView dispText;
+	TextView setColor;
+	TextView setSize;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_menu);
-        menuLabel = (TextView)findViewById(R.id.menu_label);
+        dispText = (TextView)findViewById(R.id.display_text);
+        setColor = (TextView)findViewById(R.id.set_color);
+        setSize = (TextView)findViewById(R.id.set_size);
+        registerForContextMenu(setColor);
+        registerForContextMenu(setSize);
     }
 
 
@@ -32,21 +49,48 @@ public class SimpleMenuActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.add) {
-           menuLabel.setText(R.string.add);
-        }
-        if (id == R.id.edit) {
-            menuLabel.setText(R.string.edit);
-        }
-        if (id == R.id.delete) {
-            menuLabel.setText(R.string.delete);
-        }
-        if (id == R.id.save) {
-            menuLabel.setText(R.string.save);
-        }
-        if (id == R.id.load) {
-            menuLabel.setText(R.string.load);
-        }
+
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+        ContextMenuInfo menuInfo) {
+      // TODO Auto-generated method stub
+      switch (v.getId()) {
+	    case R.id.set_color:
+	      menu.add(0, MENU_COLOR_RED, 0, "Red");
+	      menu.add(0, MENU_COLOR_GREEN, 0, "Green");
+	      menu.add(0, MENU_COLOR_BLUE, 0, "Blue");
+	      break;
+	    case R.id.set_size:
+	      menu.add(0, MENU_SIZE_22, 0, "22");
+	      menu.add(0, MENU_SIZE_26, 0, "26");
+	      menu.add(0, MENU_SIZE_30, 0, "30");
+	      break;
+    }
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+      switch (item.getItemId()) {
+    case MENU_COLOR_RED:
+    	dispText.setTextColor(Color.RED);
+      break;
+    case MENU_COLOR_GREEN:
+    	dispText.setTextColor(Color.GREEN);
+      break;
+    case MENU_COLOR_BLUE:
+    	dispText.setTextColor(Color.BLUE);
+      break;
+    case MENU_SIZE_22:
+    	dispText.setTextSize(22);
+      break;
+    case MENU_SIZE_26:
+    	dispText.setTextSize(26);
+      break;
+    case MENU_SIZE_30:
+    	dispText.setTextSize(30);
+      break;
+      }
+      return super.onContextItemSelected(item);
     }
 }
