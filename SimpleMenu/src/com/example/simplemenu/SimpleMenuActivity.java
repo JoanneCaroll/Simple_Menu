a1b22f863b17ef230ada7348595f279d33c02f86
Joanne Caroll <joannecaroll@UNIT001.(none)>
2014-10-28 16:20:41 +0800

    Simple Menu implemented.

--- SimpleMenu/src/com/example/simplemenu/SimpleMenuActivity.java
+++ SimpleMenu/src/com/example/simplemenu/SimpleMenuActivity.java
@@ -1,17 +1,21 @@
 package com.example.simplemenu;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class SimpleMenuActivity extends ActionBarActivity {
 
+	TextView menuLabel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_menu);
+        menuLabel = (TextView)findViewById(R.id.menu_label);
     }
 
 
@@ -28,8 +32,20 @@ public class SimpleMenuActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.add) {
+           menuLabel.setText(R.string.add);
+        }
+        if (id == R.id.edit) {
+            menuLabel.setText(R.string.edit);
+        }
+        if (id == R.id.delete) {
+            menuLabel.setText(R.string.delete);
+        }
+        if (id == R.id.save) {
+            menuLabel.setText(R.string.save);
+        }
+        if (id == R.id.load) {
+            menuLabel.setText(R.string.load);
         }
         return super.onOptionsItemSelected(item);
     }
